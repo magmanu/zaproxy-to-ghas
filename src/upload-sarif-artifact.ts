cc0ad796fc8e8a7c9737c4d491482187bac35255
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2024-05-24 14:30:24 +0100

    feat: add epoch to artifact name

--- src/upload-sarif-artifact.ts
+++ src/upload-sarif-artifact.ts
@@ -2,7 +2,8 @@ import {DefaultArtifactClient} from '@actions/artifact'
 
 async function uploadSarifArtifact(filename: string): Promise<void> {
   const artifactClient = new DefaultArtifactClient()
-  const artifactName = 'ZAProxy-sarif-report'
+  const now = Math.floor(new Date().getTime() / 1000)
+  const artifactName = `ZAProxy-sarif-report-${now}`
   const files = [filename]
 
   const rootDirectory = '.' // Also possible to use __dirname
