bb3f40da94e986056468d0e36556c64c4bb60638
Manu Magalhaes <89524998+magmanu@users.noreply.github.com>
2024-05-24 12:56:46 +0100

    build: Bump packages

--- src/upload-sarif-artifact.ts
+++ src/upload-sarif-artifact.ts
@@ -1,18 +1,16 @@
-import * as artifact from '@actions/artifact'
+import { DefaultArtifactClient } from '@actions/artifact'
 
 async function uploadSarifArtifact(filename: string): Promise<void> {
-  const artifactClient = artifact.create()
+  const artifactClient = new DefaultArtifactClient()
   const artifactName = 'ZAProxy-sarif-report'
   const files = [filename]
 
   const rootDirectory = '.' // Also possible to use __dirname
-  const options = {continueOnError: false}
 
   await artifactClient.uploadArtifact(
     artifactName,
     files,
-    rootDirectory,
-    options
+    rootDirectory
   )
   return
 }
